106c603435f0f2ab2677ee021b62cdcb97842fef
Shinsuke Kurosawa <veloiuria00@gmail.com>
2026-03-01 18:56:03 +0900

    feat: replace share icon with Apple-style outline (pink) and integrate ShareButton component

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -1,4 +1,5 @@
-import ShareIcon from './icons/ShareIcon'
+// src/components/ShareButton.tsx
+// Apple風「共有」アイコン（iOSライク / ピンク）
 
 type Props = {
   onClick: () => void
@@ -6,9 +7,29 @@ type Props = {
 
 export default function ShareButton({ onClick }: Props) {
   return (
-    <button className="shareButton" onClick={onClick}>
-      <ShareIcon size={18} />
-      共有
+    <button
+      className="shareBtn"
+      onClick={onClick}
+      aria-label="共有"
+      title="共有"
+    >
+      {/* Apple風 share icon */}
+      <svg
+        className="shareIcon"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        {/* 上向き矢印 */}
+        <path d="M12 3v12" />
+        <path d="M7 8l5-5 5 5" />
+
+        {/* 箱 */}
+        <path d="M5 13v5a3 3 0 003 3h8a3 3 0 003-3v-5" />
+      </svg>
     </button>
   )
 }
